a090a98c8a9c7e25da11a58940dcba3030bdbb00
yumarin <166390594+yumarin@users.noreply.github.com>
2026-03-20 17:57:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -238,5 +238,5 @@ def index():
 # 起動
 # =========================
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
